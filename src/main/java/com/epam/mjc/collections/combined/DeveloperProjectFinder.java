7a4eb61de7635004b0dfce18bfd962078ece2871
murod <murad.djanikulov@gmail.com>
2024-02-25 12:30:48 +0500

    final task done

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,19 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> result = new ArrayList<>();
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            if(entry.getValue().contains(developer)){
+                result.add(entry.getKey());
+            }
+        }
+
+        result.sort(Comparator.comparing(String::length).reversed().thenComparing(Comparator.reverseOrder()));
+
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,29 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer,Set<String>> result = new HashMap<>();
+        Map<String,Integer> helper = new HashMap<>();
+
+        for (Map.Entry<String, Integer> entry : sourceMap.entrySet()) {
+            helper.put(entry.getKey(),entry.getKey().length());
+        }
+        Set<Integer> help = new HashSet<>(helper.values());
+        for (Integer i : help) {
+            Set<String> collect = new HashSet<>();
+            for (Map.Entry<String, Integer> entry : helper.entrySet()) {
+                if (entry.getValue().equals(i)) {
+                    collect.add(entry.getKey());
+                }
+            }
+            result.put(i,collect);
+        }
+        return result;
+
     }
 }
